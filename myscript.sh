2f2b2d9583ef3fac2afd5942e4405978436dd29d
muhannadpv786 <p.vmuhannad786@gmail.com>
2025-05-04 14:26:20 +0530

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-# Install AWS CLI on Bastion Host
+set -e  # Exit on any error
+set -o pipefail
 
+echo "[INFO] Updating packages..."
 sudo apt update -y
 sudo apt install -y unzip curl
-sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-sudo unzip awscliv2.zip
+
+# Install AWS CLI v2
+echo "[INFO] Installing AWS CLI v2..."
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip -q awscliv2.zip
 sudo ./aws/install
+rm -rf aws awscliv2.zip
+echo "[INFO] AWS CLI version: $(aws --version)"
 
-# Install kubectl on Bastion Host
+# Install kubectl for EKS
+echo "[INFO] Installing kubectl..."
+curl -LO "https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.7/2024-12-12/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+echo "[INFO] kubectl version: $(kubectl version --client --short)"
 
-sudo curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.7/2024-12-12/bin/linux/amd64/kubectl && sudo chmod +x ./kubectl && sudo mv ./kubectl /usr/local/bin/kubectl
-
-
-
+echo "[INFO] Provisioning complete!"
