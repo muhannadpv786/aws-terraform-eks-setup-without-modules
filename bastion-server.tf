996932160827f52a050534768a8780f182153f3f
muhannadpv786 <p.vmuhannad786@gmail.com>
2025-05-04 14:27:14 +0530

    Update bastion-server.tf

--- bastion-server.tf
+++ bastion-server.tf
@@ -1,6 +1,5 @@
-# Create a new instance of the latest Ubuntu 14.04 on an
-# t2.micro node with an AWS Tag naming it "HelloWorld"
-
+# Launch an EC2 t2.micro instance using the latest Ubuntu 22.04 (Jammy Jellyfish)
+# and tag it as "Bastion-Server"
 
 data "aws_ami" "ubuntu" {
   most_recent = true
@@ -9,7 +8,6 @@ data "aws_ami" "ubuntu" {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
-  
 
   filter {
     name   = "virtualization-type"
@@ -19,64 +17,50 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-
 resource "aws_instance" "bastion-host" {
-  ami           = "${data.aws_ami.ubuntu.id}"         
-  instance_type = var.bastion_instance_type            
-  key_name      = aws_key_pair.tf-key-pair.key_name
-  subnet_id     = aws_subnet.public-subnet-1.id
-  vpc_security_group_ids = [
-    aws_security_group.allow_ssh.id, # Replace with your security group
-  ]
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.bastion_instance_type
+  key_name               = aws_key_pair.tf-key-pair.key_name
+  subnet_id              = aws_subnet.public-subnet-1.id
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
   tags = {
     Name = "Bastion-Server"
   }
 
-  provisioner "remote-exec" {
-    inline = [
-      "echo 'Instance is ready'",
-    ]
-
-    connection {
-      type        = "ssh"
-      user        = "ubuntu" # or ec2-user, depending on your AMI
-      private_key = tls_private_key.rsa.private_key_pem
-      host        = self.public_ip
-    }
-  }
+  # Upload the script
   provisioner "file" {
-    source      = "./myscript.sh" 
+    source      = "./myscript.sh"
     destination = "/tmp/myscript.sh"
 
     connection {
       type        = "ssh"
-      user        = "ubuntu" 
+      user        = "ubuntu"
       private_key = tls_private_key.rsa.private_key_pem
       host        = self.public_ip
     }
   }
 
-    provisioner "remote-exec" {
+  # Run the uploaded script
+  provisioner "remote-exec" {
     inline = [
       "chmod +x /tmp/myscript.sh",
-      "/tmp/myscript.sh",
+      "bash /tmp/myscript.sh"
     ]
 
     connection {
       type        = "ssh"
-      user        = "ubuntu" # or ec2-user, depending on your AMI
+      user        = "ubuntu"
       private_key = tls_private_key.rsa.private_key_pem
       host        = self.public_ip
     }
   }
 }
 
-
-
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
   description = "Allow SSH inbound traffic"
-  vpc_id      = aws_vpc.eks-vpc.id # Replace with your VPC ID
+  vpc_id      = aws_vpc.eks-vpc.id
 
   ingress {
     description = "SSH from anywhere"
